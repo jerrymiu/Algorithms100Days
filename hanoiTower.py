2dd9629a49ac6c909763b90d29c3741cfc45fe7e
Jerry Miu <jerrymiu@outlook.com>
2019-03-11 16:50:55 -0400

    Delete hanoiTower.py

--- hanoiTower.py
+++
@@ -1,9 +0,0 @@
-def hanoiTower(height, left='left', right='right', middle='middle'):
-	"""Recursively solve the tower of hanoi by moving the N-1 disks from
-	left to middle
-	height: 
-	"""
-	if height:
-		hanoiTower(height-1, left, middle, right)
-		print(left, '->', right)
-		hanoiTower(height-1, middle, right, left)
